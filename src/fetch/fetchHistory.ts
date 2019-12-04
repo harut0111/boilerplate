1f7753fa5f8a7624e5df82faada431b7f742e51c
Harutyun Muqoyan <hmuqoyan@gmail.com>
2019-12-04 18:07:31 +0400

    completed

--- src/fetch/fetchHistory.ts
+++ src/fetch/fetchHistory.ts
@@ -1,9 +1,8 @@
 import API from "../api";
 
 export default async function fetchHistory(): Promise<any> {
-    const response = await API.getHistory();
-    const history = await response.json();
-    if(history.result === "ok") return history.deals;
-    else console.log('error of fetchHistory')
+  const response = await API.getHistory();
+  const history = await response.json();
+  if (history.result === "ok") return history.deals;
+  else console.log("error of fetchHistory");
 }
-
